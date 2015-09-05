a9f3309f1454174d14a1b6e1b49ec5d08114a0ff
Anthony <pizzimentianthony@gmail.com>
2015-09-05 14:15:20 -0500

    redo constructor, search algo, getter method

--- src/Search.java
+++ src/Search.java
@@ -1,7 +1,6 @@
 
 import java.util.regex.*;
 import java.util.ArrayList;
-import javafx.scene.text.Text;
 // import javafx.scene.text.TextFlow;
 
 /**
@@ -11,53 +10,44 @@ import javafx.scene.text.Text;
 
 public class Search {
     
-    /**
-     * @param initacids list of acids as transcribed by the Codon class
-     * @param indexList list of indices at which the function finds a positive match
-     * @param origBases String of original base pairs used for comparison
-     * @param location String literal of base pair insertion locations
-     * @param length length of location
-     */
-	
-	private final String[] initacids;
-    private final ArrayList<Integer> indexList = new ArrayList<>();
-    private final Text origBases;
-	private final String location;
-	
-	public Search(String[] arr, String initlocation, String orig) {
-		initacids = arr;
-		location = initlocation;
-        origBases = new Text(orig);                         
-  	}
-	
-	public void searchforLocations() {
+    private final String location;
+    private final String context;
+    private final ArrayList<Integer> indices = new ArrayList<>();
+    
+    public Search(String init, String initContext) {
+        location = init;
+        context = initContext;
+    }
+    
+    public void findLocations() {
+        // takes out chunks of the context in lengths of 6 characters
+        String currentLocation;
+        String matcher;
         
-        // Searches through base pairs to find potential cutting spots
-		
-		for (int i = 0; i < 3; i++) {
-            char[] local = location.toCharArray();
-			local[i] = '.';
-			local[i + 3] = '.';
-
-            String matcher = new String(local);
+        for (int i = 0; i < context.length() / 6; i += 6) {
+            currentLocation = context.substring(i, i + 6);
+            
+            for (int j = 0; j < 3; j++) {
+                // generates a String for regex testing
+                char[] matchgenerator= location.toCharArray();
+                matchgenerator[j] = '.';
+                matchgenerator[j + 3] = '.';
                 
-            for (int j = 0; j < origBases.toString().length() / 6; j++) {
-                String acidforMatch = origBases.toString().substring(j, j + 6);
-                if (Pattern.matches(matcher, acidforMatch)) {
-                    indexList.add(j);
+                matcher = new String(matchgenerator);
+                
+                if (Pattern.matches(matcher, currentLocation)) {
+                    indices.add(i);
                 }
             }
         }
-	}
-
+    }
+    
     @Override
     public String toString() {
-        String returner = "";
-        
-        for(int x : indexList) {
-            returner += x + "\n";
+        String e = "";
+        for (int x : indices) {
+            e += x + ", ";
         }
-        
-        return returner;
+        return e;
     }
 }
